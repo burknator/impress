9ad4c1331420ffb0ab23b03fc14d72c278d411e3
pburke <hi@pburke.de>
2015-03-12 17:08:15 +0100

    Cleaned tests directory.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,6 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
-	public function tearDown()
-	{
-		Mockery::close();
-	}
-
 	/**
 	 * Creates the application.
 	 *
@@ -20,44 +15,4 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 		return $app;
 	}
-
-	/**
-	 * Shorthand for action('get', ...) method call.
-	 *
-	 * @param $action
-	 * @param array $parameters
-	 * @param array $server
-	 * @return \Illuminate\Http\Response
-	 */
-	public function actionGet($action, $parameters = [], $server = [])
-	{
-		return $this->action('get', $action, [], $parameters, [], $server);
-	}
-
-	/**
-	 * Shorthand for action('post', ...) method call.
-	 *
-	 * @param $action
-	 * @param array $parameters
-	 * @param array $server
-	 * @return \Illuminate\Http\Response
-	 */
-	public function actionPost($action, $parameters = [], $server = [])
-	{
-		return $this->action('post', $action, [], $parameters, [], $server);
-	}
-
-	/**
-	 * Shorthand for action('post', ...) method call.
-	 *
-	 * @param $action
-	 * @param array $parameters
-	 * @param array $server
-	 * @return \Illuminate\Http\Response
-	 */
-	public function actionPut($action, $parameters = [], $server = [])
-	{
-		return $this->action('put', $action, [], $parameters, [], $server);
-	}
-
 }
