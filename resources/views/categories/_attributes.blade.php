81a02dda6154b04100bb070d0d708a062ea2056e
Patrick Burke <hi@pburke.de>
2015-05-09 01:02:55 +0200

    Categories form now uses Laravel form builder for input elements. Created a button row ontop of categories listing, included a placeholder when list is empty.

--- resources/views/categories/_attributes.blade.php
+++ resources/views/categories/_attributes.blade.php
@@ -1,24 +1,24 @@
 <div class="category-form">
 	<div class="form-group">
-		<input type="text" name="name" id="name" class="col-md-12 form-control">
+		{!! Form::text('name', null, ['class' => 'form-control col-md-12']) !!}
 		<label for="name" class="control-label">Name your new category.</label>
 	</div>
 	<div class="form-group">
-		<input type="text" name="slug" id="slug" class="col-md-12 form-control">
+		{!! Form::text('slug', null, ['class' => 'form-control col-md-12']) !!}
 		<label for="slug" class="control-label">The URL-friendly version will be generated automatically.</label>
 	</div>
 	<div class="row">
 		<div id="color-palette" class="col-md-12">
 			@foreach($colors as $color)
 				<label for="color_{{ $color->id }}" style="background-color: #{{ $color->hex }}">
-					<input type="radio" name="color_id" id="color_{{ $color->id }}" value="{{ $color->id }}">
+					{!! Form::radio('color_id', $color->id, ['id' => 'color_' . $color->id]) !!}
 				</label>
 			@endforeach
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-12">
-			<button type="submit" class="btn btn-primary center-block">Save</button>
+			<button type="submit" class="btn btn-primary">Save</button>
 		</div>
 	</div>
 </div>
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -2,10 +2,29 @@
 
 @section('content')
     <div class="container">
-        <ul>
-        @foreach($categories as $category)
-            <li><a href="{!! route('i.categories.edit', ['category' => $category->slug]) !!}">{{ $category->name }}</a></li>
-        @endforeach
-        </ul>
+        <div class="row">
+            <div class="col-md-12 text-right">
+                <a href="{!! route('i.categories.create') !!}" class="btn btn-primary">Create new category</a>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-12">
+                @if($categories->isEmpty())
+                    <div class="content-placeholder">
+                        <h2>No categories yet</h2>
+                        <p>
+                            You didn't create any categories yet, <a href="{{ route('i.categories.create') }}">add one</a>.
+                        </p>
+                    </div>
+                @else
+                    <ul>
+                    @foreach($categories as $category)
+                        <li><a href="{!! route('i.categories.edit', ['category' => $category->slug]) !!}">{{ $category->name }}</a></li>
+                    @endforeach
+                    </ul>
+                @endif
+            </div>
+        </div>
     </div>
 @stop
