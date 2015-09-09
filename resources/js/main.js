b7839786cef46d9ea079ce61f836de2f329f80bc
Patrick Burke <hi@pburke.de>
2015-09-09 23:41:01 +0200

    Deleted CodeMirror and markdown-it files.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,21 +1,5 @@
-'use strict';
-
 +function($) {
-    if (document.getElementById('content-input') != null) {
-        $('#content-title').on('keydown keyup', function() {
-            $('#content-slug').val(this.value);
-        });
+    'use strict';
 
-        var ta = document.getElementById('content-input');
 
-        var myCodeMirror = CodeMirror.fromTextArea(ta, {
-            mode: 'markdown'
-        });
-
-        var md = window.markdownit();
-        var preview = document.getElementById('content-preview');
-        myCodeMirror.on('change', function(instance) {
-            preview.innerHTML = md.render(instance.getValue());
-        });
-    }
 }(jQuery);
