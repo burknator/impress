2f3e36577d61c43f4488b429f7d92261ad67f990
Patrick Burke <hi@pburke.de>
2015-09-18 15:04:26 +0200

    created config.json and load it in ConfigServiceProvider.

--- app/Providers/ConfigServiceProvider.php
+++ app/Providers/ConfigServiceProvider.php
@@ -8,18 +8,33 @@ class ConfigServiceProvider extends ServiceProvider
 {
 
     /**
-     * Overwrite any vendor / package configuration.
+     * Overwrite and extend app configuration.
      *
-     * This service provider is intended to provide a convenient location for you
-     * to overwrite any "vendor" or package configuration that you may want to
-     * modify before the application handles the incoming request / command.
+     * Load a user defined config.json from base path and pass the parsed contents to config()-helper.
      *
      * @return void
      */
     public function register()
     {
-        config([
-            //
-        ]);
+        $configFilePath = base_path('config.json');
+        if (!file_exists($configFilePath)) {
+            // TODO Agree on throwing Exception or give some soft feedback for the user.
+        }
+
+        $config = file_get_contents($configFilePath);
+        if ($config === false) {
+            // TODO Agree on throwing Exception or give some soft feedback for the user.
+        }
+
+        // Comments are not allowed in JSON and therefore need to be removed before decoding.
+        // Thanks @ http://php.net/manual/de/function.json-decode.php#112735
+        $config = preg_replace("#(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|([\s\t]//.*)|(^//.*)#", '', $config);
+
+        $jsonConfig = json_decode($config, true);
+        if ($jsonConfig === false) {
+            // TODO Agree on throwing Exception or give some soft feedback for the user.
+        }
+
+        config($jsonConfig);
     }
 }
