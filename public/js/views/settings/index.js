3c12c8f7681a825e0bc0b5ec95c51615f95f87f7
Patrick Burke <hi@pburke.de>
2015-09-20 23:36:18 +0200

    fixed wrong assignment of timezone setting in js

--- public/js/views/settings/index.js
+++ public/js/views/settings/index.js
@@ -1,17 +1,16 @@
 import _ from 'lodash'
+import {impress} from '/js/impress'
 import {jstz} from 'jstimezonedetect'
 import Vue from 'vue'
 
 new Vue({
     el: '#i-settings-edit',
 
-    data: _.assign({
-        // Custom models go here
-    }, _.cloneDeep(window.$data)),
+    data: impress.$data(),
 
     methods: {
         redetectTimezone: function() {
-            this['app-timezone'] = jstz.determine().name()
+            this.app.timezone = jstz.determine().name()
         }
     }
 });
