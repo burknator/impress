efabb2726191f19878e1cbd4b0ea7f457fe50e59
Patrick Burke <hi@pburke.de>
2015-09-20 17:02:06 +0200

    model factories for all available models

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -5,3 +5,48 @@ $factory->define(Impress\Author::class, function (Faker\Generator $faker) {
         'email' => $faker->email
     ];
 });
+
+$factory->define(Impress\Content::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentence,
+        'slug' => Illuminate\Support\Str::slug($faker->sentence),
+        'text' => $faker->paragraph,
+        'author_id' => factory(Impress\Author::class)->create()->id,
+        'type_id' => factory(Impress\Type::class)->create()->id,
+        'category_id' => factory(Impress\Category::class)->create()->id
+    ];
+});
+
+$factory->define(Impress\Type::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word
+    ];
+});
+
+$factory->define(Impress\Tag::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
+        'slug' => $faker->word,
+        'color_id' => factory(Impress\TagColor::class)->create()->id
+    ];
+});
+
+$factory->define(Impress\TagColor::class, function (Faker\Generator $faker) {
+    return [
+        'hex' => $faker->hexcolor
+    ];
+});
+
+$factory->define(Impress\Category::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
+        'slug' => $faker->word,
+        'color_id' => factory(Impress\CategoryColor::class)->create()->id
+    ];
+});
+
+$factory->define(Impress\CategoryColor::class, function (Faker\Generator $faker) {
+    return [
+        'hex' => $faker->hexcolor
+    ];
+});
