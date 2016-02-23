6320ef8a3e914094bbe7ae9ecd70d0882f999253
Patrick Burke <hi@pburke.de>
2016-02-23 20:51:52 +0100

    add password field to Author factory.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -2,7 +2,8 @@
 
 $factory->define(Impress\Author::class, function (Faker\Generator $faker) {
     return [
-        'email' => $faker->email
+        'email' => $faker->email,
+        'password' => $faker->password
     ];
 });
 
